bf2da051294238abf830777b82ae216a01b152e0
Ian Hank <57643516+ian-hank@users.noreply.github.com>
2023-06-25 13:52:55 -0400

    Tidying up variable assignment and user input

--- KeepComputerAwake/KeepComputerAwake.ps1
+++ KeepComputerAwake/KeepComputerAwake.ps1
@@ -15,18 +15,24 @@ $wShell = New-Object -ComObject WScript.Shell
 
 # Determine the key-press combination based on the provided or default keys
 if ($Keys) {
-    $FINAL_KEY = $Keys
-    Write-Host "`nGiven key: $FINAL_KEY"
+    try {
+        $FINAL_KEY = $Keys
+    } catch {
+        Write-Host "Invalid keypress combination input. Using the default key-press combination."
+        $FINAL_KEY = '+{F15}' # Default: Shift + F15
 } else {
-    $FIRST_KEY = '+'     # Shift
-    $SECOND_KEY = '{F15}' # F15
+    $FIRST_KEY = '+'        # Shift
+    $SECOND_KEY = '{F15}'   # F15
     $FINAL_KEY = $FIRST_KEY + $SECOND_KEY
-    Write-Host "`nDefault key-press combination: $FIRST_KEY (Shift) + $SECOND_KEY (F15)"
 }
 
 # Determine the cycle time based on the provided or default value
-$CYCLE_TIME = $Cycle
-Write-Host "Cycle time: $CYCLE_TIME seconds`n"
+try {
+    $CYCLE_TIME = $Cycle
+} catch {
+    Write-Host "Invalid cycle time input. Using the default cycle time (59 seconds)."
+    $CYCLE_TIME = 59
+}
 
 # Check if the Silent parameter was specified
 if ($Silent) {
@@ -37,11 +43,12 @@ if ($Silent) {
 } else {
     Write-Host "`nYour computer will stay awake! Use Ctrl + C to exit (or close the window)."
     Write-Host "Key combination used: $FINAL_KEY"
-    $sendKeysCount = 1
+    Write-Host "Cycle time: $CYCLE_TIME seconds`n"
 
+    $sendKeysCount = 1
     while ($true) {
         $wShell.SendKeys($FINAL_KEY)
-        Write-Host "---> Sent key combination: $FINAL_KEY [timesSent: $sendKeysCount]"
+        Write-Host "---> Sent key combination: $FINAL_KEY [Times Sent: $sendKeysCount]"
         $sendKeysCount++
         Start-Sleep -Seconds $CYCLE_TIME
     }
